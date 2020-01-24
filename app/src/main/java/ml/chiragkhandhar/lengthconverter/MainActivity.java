4f72eae472eafe79a3533029c278f75b0c651ee2
chirag khandhar <chiragkhandhar@bitbucket.org>
2020-01-24 14:40:00 -0600

    Second Commit

--- app/src/main/java/ml/chiragkhandhar/lengthconverter/MainActivity.java
+++ app/src/main/java/ml/chiragkhandhar/lengthconverter/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView ans_text;
-    private EditText kms_string;
+    private TextView dynamicLabel, dynamicAns;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void convert(View v){
-        ans_text = findViewById(R.id.ans);
-        kms_string = findViewById(R.id.kms);
-        int kms = Integer.parseInt(kms_string.getText().toString());
-        double ans;
+    public void convert(View v)
+    {
 
-        ans = kms * 0.621371;
 
-        String final_ans = ""+String.format("%.2f", ans)+" miles";
-        ans_text.setText(final_ans);
+    }
 
+    public double mtk(double miles)
+    {
+        return miles * 1.60934;
+    }
+
+    public double ktm(double kms)
+    {
+        return kms * 0.621371;
     }
 }
