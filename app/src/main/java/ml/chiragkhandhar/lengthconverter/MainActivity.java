cc05df7277198899b28908995876a2559f25a836
chirag khandhar <chiragkhandhar@bitbucket.org>
2020-01-24 20:14:08 -0600

    Module 1 Complete. (Mtk and KtM)

--- app/src/main/java/ml/chiragkhandhar/lengthconverter/MainActivity.java
+++ app/src/main/java/ml/chiragkhandhar/lengthconverter/MainActivity.java
@@ -3,26 +3,92 @@ package ml.chiragkhandhar.lengthconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView dynamicLabel, dynamicAns;
+    private TextView dynamicLabel1, dynamicLabel2, dynamicAns;
+    private RadioGroup rg;
+    private RadioButton rb, rb1,rb2;
+    private EditText et;
+    private int radioFlag;
+
+    private static final String TAG = "Radio Check";
+
+    private void fetchComponents()
+    {
+        dynamicLabel1 = findViewById(R.id.dynamicLabel1);
+        dynamicLabel2 = findViewById(R.id.dynamicLabel2);
+        dynamicAns = findViewById(R.id.dynamicAns);
+        et = findViewById(R.id.inputValue);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fetchComponents();
+        radioFlag = 1;                          // Default value
+        dynamicLabel1.setText(getString(R.string.miles));
+        dynamicLabel2.setText(getString(R.string.kms));
+        et.setHint(getString(R.string.em));
+    }
 
+    public void radioBtnClick(View v)
+    {
+        boolean checked = ((RadioButton)v).isChecked();
+
+        switch(v.getId()) {
+            case R.id.mtkBtn:
+                if (checked)
+                    dynamicLabel1.setText(getString(R.string.miles));
+                    dynamicLabel2.setText(getString(R.string.kms));
+                    et.setHint(getString(R.string.em));
+                    radioFlag = 1;
+                    break;
+            case R.id.ktmBtn:
+                if (checked)
+                    dynamicLabel2.setText(getString(R.string.miles));
+                    dynamicLabel1.setText(getString(R.string.kms));
+                    et.setHint(getString(R.string.ek));
+                    radioFlag = 2;
+                    break;
+        }
     }
 
     public void convert(View v)
     {
-
+        if (et.getText().toString().equals(""))
+        {
+            et.setError("Required");
+        }
+        else
+        {
+            double miles, kms;
+            String finalAns;
+            switch (radioFlag)
+            {
+                case 1:
+                    miles = Double.parseDouble(et.getText().toString());
+                    kms = mtk(miles);
+                    finalAns = String.format("%.1f",kms);
+                    dynamicAns.setText(finalAns);
+                    break;
+                case 2:
+                    kms = Double.parseDouble(et.getText().toString());
+                    miles = ktm(kms);
+                    finalAns = String.format("%.1f",miles);
+                    dynamicAns.setText(finalAns);
+                    break;
+            }
+        }
 
     }
 
@@ -34,5 +100,11 @@ public class MainActivity extends AppCompatActivity {
     public double ktm(double kms)
     {
         return kms * 0.621371;
+    }
+
+    public void clear(View view)
+    {
+        dynamicAns.setText("");
+        et.setText("");
     }
 }
